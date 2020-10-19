d62cba745d18c6b5cfc8fdf1ab1de24b4794c26c
452MJ <447180005@qq.com>
2020-10-19 22:54:17 +0800

    chore: drawer（未完成）

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,70 +1,97 @@
 // Custom DatePicker that uses Day.js instead of Moment.js
-import React from 'react'
-import { Dropdown, Menu } from 'antd'
+import React, { useState } from 'react'
+import { Drawer, Dropdown, Menu } from 'antd'
 
 export default function Nav() {
+  const [visible, setVisible] = useState(false)
+  const showDrawer = () => {
+    setVisible(true)
+  }
+  const onClose = () => {
+    setVisible(false)
+  }
   return (
-    <div
-      className="row main-between"
-      style={{
-        backgroundColor: '#752EA4',
-        width: '100%',
-        height: 63,
-        paddingLeft: 60,
-        paddingRight: 90,
-      }}
-    >
-      <div style={{ width: 80, height: 48, backgroundColor: 'white' }} />
+    <>
+      <div
+        className="row main-between"
+        style={{
+          backgroundColor: '#752EA4',
+          width: '100%',
+          height: 63,
+          paddingLeft: 60,
+          paddingRight: 90,
+        }}
+      >
+        <div style={{ width: 80, height: 48, backgroundColor: 'white' }} />
 
-      <div className="row main-between">
-        <Dropdown
-          overlay={
-            <Menu style={{ top: 20, right: 100 }}>
-              <Menu.Item key="0">
-                <span
-                  style={{
-                    width: 125,
-                    height: 44,
-                    fontSize: 16,
-                    fontWeight: 'bold',
-                    color: '#333',
-                  }}
-                >
-                  Leo zhang
-                </span>
-              </Menu.Item>
-              <Menu.Divider />
-              <Menu.Item key="1">
-                <span
-                  style={{
-                    width: 125,
-                    height: 44,
-                    fontSize: 16,
-                    fontWeight: 500,
-                    color: '#333',
-                  }}
-                >
-                  Drop out
-                </span>
-              </Menu.Item>
-            </Menu>
-          }
-          trigger={['click']}
-        >
-          <div
-            className="mouse"
-            style={{
-              width: 34,
-              height: 34,
-              backgroundColor: 'white',
-              borderRadius: '50%',
-              marginRight: 43,
-            }}
+        <div className="row main-between">
+          <Dropdown
+            overlay={
+              <Menu style={{ top: 20, right: 100 }}>
+                <Menu.Item key="0">
+                  <span
+                    style={{
+                      width: 125,
+                      height: 44,
+                      fontSize: 16,
+                      fontWeight: 'bold',
+                      color: '#333',
+                    }}
+                  >
+                    Leo zhang
+                  </span>
+                </Menu.Item>
+                <Menu.Divider />
+                <Menu.Item key="1">
+                  <span
+                    style={{
+                      width: 125,
+                      height: 44,
+                      fontSize: 16,
+                      fontWeight: 500,
+                      color: '#333',
+                    }}
+                  >
+                    Drop out
+                  </span>
+                </Menu.Item>
+              </Menu>
+            }
+            trigger={['click']}
+          >
+            <div
+              className="mouse"
+              style={{
+                width: 34,
+                height: 34,
+                backgroundColor: 'white',
+                borderRadius: '50%',
+                marginRight: 43,
+              }}
+            />
+          </Dropdown>
+
+          <button
+            type="button"
+            style={{ width: 39, height: 34, backgroundColor: 'white' }}
+            onClick={showDrawer}
           />
-        </Dropdown>
-
-        <div style={{ width: 39, height: 34, backgroundColor: 'white' }} />
+        </div>
       </div>
-    </div>
+      <Drawer
+        placement="right"
+        closable={false}
+        onClose={onClose}
+        visible={visible}
+        maskStyle={{ background: '#fff' }}
+        width={875}
+        style={{ opacity: 0.96 }}
+        bodyStyle={{ background: '#752EA4' }}
+      >
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+        <p>Some contents...</p>
+      </Drawer>
+    </>
   )
 }
--- pages/home/index.jsx
+++ pages/home/index.jsx
@@ -1,18 +1,43 @@
 // Custom DatePicker that uses Day.js instead of Moment.js
 import React from 'react'
+import { Layout, Image } from 'antd'
 import Nav from '../../components/Nav'
 
-export default function Home() {
-  return (
-    <div>
-      <Nav />
-      <img
-        src="/home1.webp"
-        alt=""
-        // width={1000}
-        // height={1000}
-        style={{ width: '100%' }}
-      />
-    </div>
-  )
+const { Header, Footer, Sider, Content } = Layout
+
+export default class Home extends React.Component {
+  state = {
+    collapsed: false,
+  }
+
+  onCollapse = collapsed => {
+    console.log(collapsed)
+    this.setState({ collapsed })
+  }
+
+  render() {
+    return (
+      <Layout>
+        <Header
+          style={{
+            height: 63,
+            padding: 0,
+            margin: 0,
+            border: 0,
+          }}
+        >
+          <Nav />
+        </Header>
+        <Content>
+          <img
+            src="/home1.webp"
+            alt=""
+            width={'100%'}
+            // height={1000}
+            // style={{ width: '100%' }}
+          />
+        </Content>
+      </Layout>
+    )
+  }
 }
